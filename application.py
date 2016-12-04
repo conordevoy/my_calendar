e84fbcf51ff4cda3dc5c40dde88bf9b7bafa6c50
Conor Devoy <conor.devoy@ucdconnect.ie>
2016-12-04 21:56:20 +0000

    a heroku app

---
+++ application.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, render_template
+
+application = Flask(__name__)
+
+@application.route('/')
+def calendar():
+    return render_template("index.html")
+
+@app.route('/data')
+def return_data():
+    start_date = request.args.get('start', '')
+    end_date = request.args.get('end', '')
+
+    with open("data.json", "r") as input_data:
+				return input_data.read()
+
+if __name__ == "__main__":
+    application.debug = True
+    application.run()
